b69580039113c8a6dc108780dac8f66ebd261fd1
highlyko17@gmail.com <61534328+HyeliKo@users.noreply.github.com>
2021-05-06 17:06:14 +0900

    mile to Km and Ton to kg

---
+++ app/src/main/java/edu/handong/csee/java/hw2/converters/TONToKGConverter.java
@@ -0,0 +1,33 @@
+package edu.handong.csee.java.hw2.converters;
+import java.math.BigDecimal;
+
+public class TONToKGConverter implements Convertible {
+	private double num;
+	private double convertedNum;
+
+	/**
+		*setting a value to convert
+	*/
+	@Override
+	public void setFromValue(double fromValue){
+		num = fromValue;
+	}
+
+	/**
+		*getting a converted value
+	*/
+	@Override
+	public double getConvertedValue(){
+		return convertedNum;
+	}
+
+	/**
+		*converting value from ton to kg
+	*/
+	@Override
+	public void convert(){
+		BigDecimal b1 = new BigDecimal(String.valueOf(num));
+		BigDecimal b2 = new BigDecimal("1000");
+		convertedNum = b1.multiply(b2).doubleValue();
+	}
+}
